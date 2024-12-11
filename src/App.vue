abb7aecf7e1ee0363988d85883cef097ccadf2a4
Justin Breton <breton.justin97@gmail.com>
2024-12-11 18:20:39 -0500

    Added first combobox

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import Select from 'primevue/select';
+
+const selectedCity = ref(null);
+const categories = ref([
+  { category: 'Grammaire' },
+  { category: 'Orthographe' },
+  { category: 'Accord' },
+]);
 </script>
 
 <template>
-  <div class="container mx-auto">Hello world</div>
+  <div class="container mx-auto mt-4">
+    <Select v-model="selectedCity" :options="categories" optionLabel="category" placeholder="Choisir une catégorie" class="w-full md:w-56" />
+  </div>
 </template>
 
 <style scoped></style>
