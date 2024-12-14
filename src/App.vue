8be1efd9a803f59508e14ba0740dd5a681c91ac0
Justin Breton <breton.justin97@gmail.com>
2024-12-13 22:17:58 -0500

    Almost working

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,41 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import Select from 'primevue/select';
+import RowComponent from './components/RowComponent.vue'
+import Button from 'primevue/button'
 
-const selectedCity = ref(null);
-const categories = ref([
-  { category: 'Grammaire' },
-  { category: 'Orthographe' },
-  { category: 'Accord' },
-]);
+const rows = ref([{}]) // Start with one row
+
+const handleAddClick = () => {
+  rows.value.push({}) // Add a new item to the list
+}
+
+const handleRowRemove = (index: number) => {
+  if (rows.value.length === 1) {
+    return // Do not remove the last row
+  }
+  rows.value.splice(index, 1)
+}
+
+const handlePrint = () => {
+  window.print()
+}
 </script>
 
 <template>
-  <div class="container mx-auto mt-4">
-    <Select v-model="selectedCity" :options="categories" optionLabel="category" placeholder="Choisir une catégorie" class="w-full md:w-56" />
+  <div class="container mx-auto mt-4 print:mx-0 print:mt-0">
+    <h1 class="text-2xl font-bold py-2">Méthode de correction</h1>
+    <div id="rowList">
+      <RowComponent v-for="(row, index) in rows" :key="index" @remove="handleRowRemove(index)" />
+    </div>
+    <div class="float-right space-x-4 print:hidden">
+      <Button
+        type="button"
+        label="Imprimer"
+        icon="pi pi-print"
+        @click="handlePrint"
+        severity="info"
+      />
+      <Button rounded icon="pi pi-plus" aria-label="Add" @click="handleAddClick" />
+    </div>
   </div>
 </template>
-
-<style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,19 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import PrimeVue from 'primevue/config'
-import Aura from '@primevue/themes/aura';
+import Aura from '@primevue/themes/aura'
+import 'primeicons/primeicons.css'
 
 const app = createApp(App)
 app.use(PrimeVue, {
   theme: {
-    preset : Aura,
-    options:  {
+    preset: Aura,
+    options: {
       prefix: 'p',
       darkModeSelector: '',
-      cssLayer: false
-    }
-  }
+      cssLayer: false,
+    },
+  },
 })
 
 app.mount('#app')
